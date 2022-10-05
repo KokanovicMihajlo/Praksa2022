6be2dd0da4192d096e91f7a0b53079c2ddf90408
Kokanovic.Mihajlo <kokankg@gmail.com>
2022-10-05 14:53:14 +0200

    Refactored code

---
+++ cypress/e2e/features/ListCurrentAccounts.feature
@@ -0,0 +1,8 @@
+Feature: List Current Accounts
+
+    As a user I want to see my current account list
+
+    Scenario: As a user I want to see my current account list
+        Given I am loged in
+        When  I am on My Products page
+        Then  I shoud see necessary properties
---
+++ cypress/e2e/step_definitions/ListCurrentAccounts.js
@@ -0,0 +1,19 @@
+import {
+    Given,
+    When,
+    And,
+    Then,
+  } from "@badeball/cypress-cucumber-preprocessor"
+  
+When('I am on My Products page',()=>{
+  cy.get('[data-role=headings]').should('contain', 'My products')
+})
+
+Then('I shoud see necessary properties',()=>{
+  cy.get('[data-role="Current Accounts"]').should('contain','Current Accounts')
+  cy.get('[class="bb-product-kind__balance-title mr-2"]').should('contain','Total')
+  cy.get('[data-role=current-account-total-amount]').should('contain','8 145', ',', '91', 'BGN')
+  cy.get('[data-role=card-title]').should('contain','Current Account')
+  cy.get('[data-role=card-sub-title]').should('contain','BG23STSA93000005794456')
+  cy.get('[data-role=bb-amount-value__amount]').should('contain','1 234', ',', '23', 'BGN')
+})
