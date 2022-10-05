21a882a70918f746b6f605bb361a9ae020dddc3e
Kokanovic.Mihajlo <kokankg@gmail.com>
2022-10-05 15:11:43 +0200

    Update ListCurrentAccounts.js

--- cypress/e2e/step_definitions/ListCurrentAccounts.js
+++ cypress/e2e/step_definitions/ListCurrentAccounts.js
@@ -11,9 +11,9 @@ When('I am on My Products page',()=>{
 
 Then('I shoud see necessary properties',()=>{
   cy.get('[data-role="Current Accounts"]').should('contain','Current Accounts')
-  cy.get('[class="bb-product-kind__balance-title mr-2"]').should('contain','Total')
+  cy.get('[class=bb-product-kind__balance]').should('contain','Total')
   cy.get('[data-role=current-account-total-amount]').should('contain','8 145', ',', '91', 'BGN')
   cy.get('[data-role=card-title]').should('contain','Current Account')
   cy.get('[data-role=card-sub-title]').should('contain','BG23STSA93000005794456')
-  cy.get('[data-role=bb-amount-value__amount]').should('contain','1 234', ',', '23', 'BGN')
+  cy.get('[data-role=bb-amount-value]').eq(1).should('contain','1 234', ',', '23', 'BGN')
 })
