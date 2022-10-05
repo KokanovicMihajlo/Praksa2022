7b02539d1f3ad85eb5cb784861d76e6eeb288452
Kokanovic.Mihajlo <kokankg@gmail.com>
2022-10-05 14:54:09 +0200

    Refactor

--- cypress.config.js
+++ cypress.config.js
@@ -5,18 +5,6 @@ const addCucumberPreprocessorPlugin =
 const createEsbuildPlugin =
   require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
-//If using this approach, just call the key "setupNodeEvents" in the E2E configurations
-// async function setupNodeEvents(on, config) {
-//   await addCucumberPreprocessorPlugin(on, config);
-//   on(
-//     "file:preprocessor",
-//     createBundler({
-//       plugins: [createEsbuildPlugin(config)],
-//     })
-//   );
-//   return config;
-// }
-
 module.exports = defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
---
+++ cypress/e2e/features/ListCurrentAccounts.feature
@@ -0,0 +1,8 @@
+Feature: Curent Login
+
+    As a user I want to see my current account list
+
+    Scenario: As a user I want to see my current account list
+        Given I am loged in
+        When  I am on My Products Page
+        Then  I shoud see necessary properties
